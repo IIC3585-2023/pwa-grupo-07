948eb219119cc974daad8f69331febc589fb06bf
Juan Pablo Anwandter <janwandter@uc.cl>
2023-05-22 17:45:01 -0400

    fix typo

--- js/overview.js
+++ js/overview.js
@@ -1,31 +1,3 @@
-const events = [
-  {
-    'NTkkphmuTThuhRk-bxD': {
-      name: 'nameTest',
-      participants: { 0: 'Benja', 1: 'Fran', 2: 'JP' },
-      transactions: [
-        {
-          name: 'nameTest',
-          ammount: 30,
-          payer: 0,
-          participants: [0, 1, 2],
-          date: '2023-05-19',
-        },
-        {
-          name: 'nameTest2',
-          ammount: 20,
-          payer: 1,
-          participants: [0, 1],
-          date: '2023-05-21',
-        },
-      ],
-      totalSpending: 60,
-      payments: { 0: 20, 1: 40, 2: 0 },
-    },
-  },
-];
-
-let currentEvent;
 let currentUser;
 
 function generateEventId() {
